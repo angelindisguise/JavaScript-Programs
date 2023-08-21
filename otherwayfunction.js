be2ff766549b58bdf11e1bf90b85ab764ffb82f2
angelindisguise <76496683+angelindisguise@users.noreply.github.com>
2023-08-21 11:27:33 +0100

    s

--- otherwayfunction.js
+++ otherwayfunction.js
@@ -6,6 +6,20 @@ const anthrFnctn = function() {
     console.log("another thing");
 }
 
+const returnFunction = () => {
+    const a = 21;
+    return a;
+}
+
+const Function1 = () => {
+    const b = 21;
+    return b > a;
+}
+
 console.log(typeof anthrFnctn);
 anthrFnctn();
 myFunction();
+const result = returnFunction();
+result;
+const result2 = Function1();
+result2;
