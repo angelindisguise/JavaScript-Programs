bc760654cc7cebe474f54314811b17918e532a78
angelindisguise <76496683+angelindisguise@users.noreply.github.com>
2023-08-21 11:57:53 +0100

    hu

--- inbuiltFuncs.js
+++ inbuiltFuncs.js
@@ -1,4 +1,13 @@
 // inbuilt functions
 const myString = "Janes";
+const rightName = myString.replace("n", "m");
+upperC = rightName.toUpperCase();
+lowerC = myString.toLowerCase();
+let tempstr = "yamama";
 
-myString.replace("n", "m");
+console.log("Wrong name:", myString);
+console.log("Right name:", rightName);
+console.log("Upper case:", upperC);
+console.log("Lower case:", myString);
+const modTempStr = tempstr.toUpperCase().split("");
+console.log(modTempStr);
