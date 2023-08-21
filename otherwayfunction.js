b484b7eaad24195130b6161354785e337247d217
angelindisguise <76496683+angelindisguise@users.noreply.github.com>
2023-08-21 11:19:38 +0100

    h

---
+++ otherwayfunction.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    console.log("smthn");
+}
+
+const anthrFnctn = function() {
+    console.log("another thing");
+}
+
+console.log(typeof anthrFnctn);
+anthrFnctn();
+myFunction();
